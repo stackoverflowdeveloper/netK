1fcdcfbec2645406f9ad650447e4478f28c74173
stackoverflowdeveloper <ingratus.org@gmail.com>
2018-01-26 07:51:47 +0100

    start

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('reload', function(){
+	browserSync.reload();
+})	
+
+
+gulp.task('serve', ['sass'], function(){
+browserSync({
+	server: 'src'
+	});
+
+
+	gulp.watch('src/*.html').on('change', browserSync.reload);
+	gulp.watch('src/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function(){
+	return gulp.src('src/scss/**/*.scss')
+		.pipe(sourcemaps.init())
+
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 3 versions']
+		}))
+		.pipe(sourcemaps.write())
+		.pipe(gulp.dest('src/css'))
+		.pipe(browserSync.stream());
+})
+
+
+
+gulp.task('default', function(){
+	console.log('lol default');
+});
